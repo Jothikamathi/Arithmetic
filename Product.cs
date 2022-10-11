3f50d11154c95bd2b465075ae714ee31a66f8ce3
Allsec <Allsec@ASTLPTWFH155>
2022-10-11 17:53:45 +0530

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic
+{
+    public class Product
+    {
+        //properties
+
+        int ProductID;
+        string ProductName;
+        int Mrp_Price;
+        float Gst=13.2f;
+        float Discount;
+        float AfterDiscount;
+        int Count;
+        int Total;
+        float FinalTotal;
+
+
+        //methods
+
+        public void AddProducts()
+        {
+            Console.WriteLine("Enter ProductID : ");
+            ProductID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter ProductName");
+            ProductName = Console.ReadLine();
+            Console.WriteLine("Enter Mrp_Price : ");
+            Mrp_Price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Discount:");
+            Discount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many products do you want to Buy: ");
+            Count = Convert.ToInt32(Console.ReadLine());
+            Total = Mrp_Price * Count;
+            AfterDiscount = Total - (Total * Discount / 100);
+            FinalTotal = (AfterDiscount * Gst) / 100;
+            Console.WriteLine($"You have to pay amount is: {FinalTotal} ");
+            Console.WriteLine("------------------");
+            Console.WriteLine("Thank You For Shopping with US!");
+         } 
+        public void DisplayProductDetails()
+        {
+            Console.WriteLine($" {Total} {AfterDiscount} {FinalTotal} ");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿/*using System;
+namespace MySampleProgramOOPS
+{
+    class Program
+    {
+        //entry point of application
+        public static void Main()
+        {
+            /*Array Example
+            int[] marks = new int[5];
+            marks[0] = 22;
+            marks[1] = 24;
+            marks[2] = 25;
+            marks[3] = 26;
+            marks[4] = 30;
+
+            //Looping
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine("Element of marks  array [{0}] has {1} ", i, marks[i]);
+            }
+        }
+    }
+}*/
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+using Arithmetic;
+using System;
+namespace SampleProgram
+{
+    class Program
+    {
+        //entry point of application
+        public static void Main()
+        {
+            Product pro = new Product();
+            pro.AddProducts();
+            pro.DisplayProductDetails();
+            Console.ReadKey();
+        }
+    }
+}
